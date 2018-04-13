d02fdc7370f1e258c799acd7d63d001b0a15b191
Anamul Islam <anamul.islam127@gmail.com>
2018-04-13 00:31:36 -0400

    Pushing lab 2

---
+++ Lab2Dates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a date in MM/DD/YYYY format.")
+            Console.ReadKey();
+        }
+    }
+}
